56e2b97782a429542b03c1ea364dfb4e7234eebf
Bo.Cui <Bo.Cui@noaa.gov>
2025-09-16 16:26:11 -0400

    add source code hgefs_ensstat to hgefs package

---
+++ sorc/build_hgefs_ensstat.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/env bash
+set -eux
+
+#source ./machine-setup.sh
+#target=wcoss2
+module reset
+cwd=`pwd`
+
+progname=hgefs_ensstat
+
+source ../versions/build.ver
+
+module use ${cwd}/../modulefiles/hgefs
+module load ${progname} #.${target}
+module list
+
+# Check final exec folder exists
+if [ ! -d "../exec" ]; then
+  mkdir ../exec
+fi
+
+#
+cd ${progname}.fd
+
+export FCMP=${FCMP:-ftn}
+export FCMP95=$FCMP
+
+export FFLAGSM="-O3 -traceback -convert big_endian"
+export RECURS=
+export LDFLAGSM=${LDFLAGSM:-""}
+export OMPFLAGM=${OMPFLAGM:-""}
+
+export INCSM="-I ${G2_INC4}"
+
+export LIBSM="${G2_LIB4} ${W3NCO_LIB4} ${BACIO_LIB4} ${JASPER_LIB} ${PNG_LIB} ${Z_LIB}"
+
+make -f Makefile clobber
+make -f Makefile
+make -f Makefile install
+make -f Makefile clobber
+
+exit
