c06cbe6ad50d7d92ef2d623edbcaaa9f9fde5120
Bo.Cui <Bo.Cui@noaa.gov>
2025-10-20 15:49:44 -0400

    Improve build script, makefile, and add READM, remove unused files

--- sorc/build_hgefs_ensstat.sh
+++ sorc/build_hgefs_ensstat.sh
@@ -1,8 +1,20 @@
 #! /usr/bin/env bash
+
+#######################################################################
+### Usage:
+###
+### To build, install and clean executable hgefs_enssat
+###  ./build_hgefs_ensstat.sh >& hgefs.compile.log
+###
+### To build, install and clean executable hgefs_enssate in debug mode
+###  ./build_hgefs_ensstat.sh debug >& hgefs.compile.log  
+###
+###  More information in README.build
+###
+#######################################################################
+
 set -eux
 
-#source ./machine-setup.sh
-#target=wcoss2
 module reset
 cwd=`pwd`
 
@@ -26,7 +38,13 @@ export FCMP=${FCMP:-ftn}
 export FCMP95=$FCMP
 
 export FFLAGSM="-O3 -traceback -convert big_endian"
-#export FFLAGSM="-O3 -traceback -convert big_endian -check all -ftrapuv"
+
+# Check if we are building debug executable
+if [ "${1:-}" = "debug" ]; then
+  export FFLAGSM="-O3 -traceback -convert big_endian -check all -ftrapuv"
+  echo "Building in debug mode"
+fi
+
 export RECURS=
 export LDFLAGSM=${LDFLAGSM:-""}
 export OMPFLAGM=${OMPFLAGM:-""}
@@ -35,9 +53,17 @@ export INCSM="-I ${G2_INC4}"
 
 export LIBSM="${G2_LIB4} ${W3NCO_LIB4} ${BACIO_LIB4} ${JASPER_LIB} ${PNG_LIB} ${Z_LIB}"
 
-make -f Makefile clobber
-make -f Makefile
-make -f Makefile install
-make -f Makefile clobber
+if [ "${1:-}" = "clean" ]; then
+  make -f Makefile clean
+elif [ "${1:-}" = "build" ]; then
+  make -f Makefile
+elif [ "${1:-}" = "install" ]; then
+  make -f Makefile install
+else
+  make -f Makefile clobber
+  make -f Makefile
+  make -f Makefile install
+  make -f Makefile clobber
+fi
 
 exit
